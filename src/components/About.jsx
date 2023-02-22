0cea936cc298414b613dac4a4ef8035684eac1e3
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2023-02-23 00:22:54 +0200

    updated src code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
       <p><b>Exodia OS</b> is a highly customized <b>Arch-based</b> Linux distribution built for and by cybersecurity experts, and also for daily usage, capable of performing the strongest <b>pentests</b> and the simplest <b>daily tasks</b>. Even if you're not a cybersecurity fanatic, you'll come to love Exodia OS for it's <b>aesthetically pleasing</b> interface, tools, and features.  Overall, Exodia OS is a great distro choice for <b>programming</b> and development, operations security, OSINT, and of course <b>daily usage</b>. <br/><br/> Proudly developed in <b>Egypt 🇪🇬</b> by <b>Cyb3rTh1eveZ</b> Team. </p>
       <h2>Benefits:</h2>
       <ul>
-        <li>Exodia OS Comes with <b>BSPWM</b> (a tiling window manager) and 18 tailor-made fancy <b>Themes</b>.</li>
+        <li>Exodia OS Comes with <b>BSPWM</b> (a tiling window manager) and 20 tailor-made fancy <b>Themes</b>.</li>
         <li><b>Cybersecurity tools</b> are preinstalled on Exodia OS, for all tracks of cybersecurity.</li>
         <li>Currently, 3 <b>Editions</b> exist: <b>Home</b>, <b>Predator</b>, and <b>Wireless</b>.</li>
         <li>Currently, 2 <b>window managers</b> are available: <b>BSPWM</b> and <b>DWM</b>, and we will add more <b>DEs</b> and <b>WMs</b>.</li>
